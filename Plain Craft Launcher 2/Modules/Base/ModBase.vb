07947266732367eb616fb7dce2369b2a19fb2960
tangge233 <tangge233@qq.com>
2025-03-08 19:30:49 +0800

    bump: 2.9.1.2

--- Plain Craft Launcher 2/Modules/Base/ModBase.vb
+++ Plain Craft Launcher 2/Modules/Base/ModBase.vb
@@ -12,8 +12,8 @@ Public Module ModBase
 #Region "声明"
 
     '下列版本信息由更新器自动修改
-    Public Const VersionBaseName As String = "2.9.1.1" '不含分支前缀的显示用版本名
-    Public Const VersionStandardCode As String = "2.9.1.1" & VersionBranchCode '标准格式的四段式版本号
+    Public Const VersionBaseName As String = "2.9.1.2" '不含分支前缀的显示用版本名
+    Public Const VersionStandardCode As String = "2.9.1.2" & VersionBranchCode '标准格式的四段式版本号
     Public Const CommitHash As String = "" 'Commit Hash，由 GitHub Workflow 自动替换
 #If BETA Then
     Public Const VersionCode As Integer = 352 'Release
