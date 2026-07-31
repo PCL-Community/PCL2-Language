c2c3c84b5086b69d1dd4913e900a952eafa71d96
tangge233 <tangge233@qq.com>
2026-07-31 20:42:32 +0800

    bump: 2.13.0.2

--- Plain Craft Launcher 2/Modules/ModMain.vb
+++ Plain Craft Launcher 2/Modules/ModMain.vb
@@ -974,7 +974,7 @@ NextFile:
         If ForceOpenLog OrElse (ShowMsgbox AndAlso MyMsgBox("若你在汇报一个 Bug，请点击 打开文件夹 按钮，并上传 Log(1~5).txt 中包含错误信息的文件。" & vbCrLf & "游戏崩溃一般与启动器无关，请不要因为游戏崩溃而提交反馈。", "反馈提交提醒", "打开文件夹", "不需要") = 1) Then
             OpenExplorer(Paths.Base & "PCL\Log1.txt")
         End If
-        OpenWebsite("https://github.com/Meloong-Git/PCL/issues/")
+        OpenWebsite("https://github.com/PCL-Community/PCL2-Language/issues/new/choose")
     End Sub
     ''' <summary>
     ''' 在日志中输出系统诊断信息。
